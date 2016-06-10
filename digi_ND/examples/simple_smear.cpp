a158a7366b61da98be6ae69b1e4924d04f0091a5
patha325 <p.hallsjo.1@research.gla.ac.uk>
2016-06-10 11:53:22 +0100

    first attempt at handling test_beam data

--- digi_ND/examples/simple_smear.cpp
+++ digi_ND/examples/simple_smear.cpp
@@ -21,7 +21,247 @@
 
 #include <stdio.h>
 
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <bitset> 
+#include <iostream>
+#include <stdlib.h> 
+
+#include <locale>
+
+#include <libxml/parser.h>
+#include <libxml/tree.h>
+
 using namespace std;
+
+class SpillHeader
+{
+public:
+  SpillHeader() {};
+  SpillHeader(unsigned long int in)
+  {
+    id = (in >> 28)& 0xF;
+    boardId = (in >> 20) & 0x7F;
+    spillTag = in & 0xFFFF;
+      
+  };
+  unsigned int id;
+  unsigned int boardId;
+  unsigned int spillTag;
+};
+
+class EventHeader
+{
+public:
+  EventHeader() {};
+  EventHeader(unsigned long int in)
+  {
+    id = (in >> 28)& 0xF;
+    triggerTag = in & 0xFFFFFFF; //27-0
+
+  };
+  unsigned int id;
+  unsigned long int triggerTag;
+};
+
+class HitAmplitude
+{
+public:
+  HitAmplitude() {};
+  HitAmplitude(unsigned long int in)
+  {
+    hitAmplitudeId = (in >> 28)& 0xF;
+    channelId = (in >> 21)& 0x7F;
+    hitId = (in >> 16)& 0x1F;
+    amplitudeId = (in >> 12)& 0xF;
+    amplitudeMeasurement = in & 0xFFF;
+  };
+  unsigned int hitAmplitudeId;
+  unsigned int channelId;
+  unsigned int hitId;
+  unsigned int amplitudeId;
+  unsigned int amplitudeMeasurement;
+};
+
+class HitTime
+{
+public:
+  HitTime() {};
+  HitTime(vector<unsigned long int> inV)
+  {
+    hitTimeId = (inV[0] >> 28)& 0xF;
+    channelId = (inV[0] >> 21)& 0x7F;
+    hitId = (inV[0] >> 16)& 0x1F;
+    hitTime  = inV[0]& 0xFFFF;
+
+    if(inV.size() <2)
+      {
+	cout<<"No hits!!!"<<endl;
+      }
+    else
+      {
+	vector<unsigned long int> ampV(inV.begin()+1,inV.end());
+	for(unsigned int cnt=0;cnt<ampV.size();cnt++)
+	  {
+	    hitAmplitudeVec.push_back(HitAmplitude(ampV[cnt]));
+	  }
+      }
+  };
+  unsigned int hitTimeId;
+  unsigned int channelId;
+  unsigned int hitId;
+  unsigned int hitTime;
+
+  vector<HitAmplitude> hitAmplitudeVec;
+
+};
+
+class EventTrailer
+{
+public:
+  EventTrailer() {};
+  EventTrailer(vector<unsigned long int> inV)
+  {
+    //unsigned int temp[] = {(inV[0] << 28)& 0xF,(inV[1] << 28)& 0xF};
+    //eventTrailerId = temp;
+    //memcpy(&eventTrailerId, &temp, sizeof eventTrailerId);
+
+    //eventTrailerId = {(inV[0] << 28)& 0xF,(inV[1] << 28)& 0xF};
+    triggerTag = inV[0] & 0xFFFFFFF;
+    hitInEvt = (inV[1] >> 23)& 0x1F;
+    id = (inV[1] >> 19)& 0x3;
+    triggerTime = inV[1] & 0x3FFFF;
+
+  };
+  //unsigned int eventTrailerId[2];
+  unsigned long int triggerTag;
+  unsigned int hitInEvt;
+  unsigned int id;
+  unsigned int triggerTime;
+};
+
+class SpillTrailer
+{
+public:
+  SpillTrailer() {};
+  SpillTrailer(vector<unsigned long int> inV)
+  {
+    //unsigned int temp[] = {(inV[0] << 28)& 0xF,(inV[1] << 28)& 0xF,(inV[2] << 28)& 0xF};
+    //unsigned int temp2[] = {(inV[0] << 21)& 0x7F,(inV[1] << 21)& 0x7F};
+
+    //spillTrailerId = temp;
+    //boardId =temp2;
+
+    //memcpy(&spillTrailerId, &temp, sizeof spillTrailerId);
+    //memcpy(&boardId, &temp2, sizeof boardId);
+
+  //spillTrailerId = {(inV[0] << 28)& 0xF,(inV[1] << 28)& 0xF,(inV[2] << 28)& 0xF};
+  //boardId = {(inV[0] << 21)& 0x7F,(inV[1] << 21)& 0x7F};
+
+    spillTag = inV[0] & 0xFFFF;
+    temperature = (inV[1] >> 8)& 0x3FF;
+    humidity = inV[1] & 0xFF;
+    spillTime = inV[2] & 0xFFFFFFF;
+
+  };
+  //unsigned int spillTrailerId[3];
+  //unsigned int boardId[2];
+  unsigned int spillTag;
+  unsigned int temperature;
+  unsigned int humidity;
+  unsigned long int spillTime;
+
+};
+
+class HitStructure
+{
+public:
+  HitStructure() {};
+  HitStructure(vector<unsigned long int> longV)
+  {
+    if(longV.size() < 8)
+      {
+      cout<<"No hit info"<<endl;
+      }
+    else
+      {
+
+    cout<<"Starting constructor"<<endl;
+    header = SpillHeader(longV[0]);
+    cout<<"Created SpillHeader"<<endl;
+    eventHeader = EventHeader(longV[1]);
+    cout<<"Created EventHeader"<<endl;
+    triggerTag = longV[1] & 0xFFFFFFF; //27-0
+
+    vector<unsigned long int> hitsV(longV.begin()+2,longV.end()-5);
+    /*
+    std::cout << "HITID = " << longV[2]<< std::endl;
+    std::cout << "HITID = " << std::bitset<32>((longV[2])) << std::endl;
+    std::cout << "HITID = " << std::bitset<5>((longV[2] >> 16) & 0x1F) << std::endl;
+
+    std::cout << "HITID = " << std::bitset<32>((longV[3])) << std::endl;
+    std::cout << "HITID = " << std::bitset<5>((longV[3] >> 16) & 0x1F) << std::endl;
+    */
+    // Create hitTimeVec.
+    // When hit id changes, new hit.
+
+    unsigned int baseHitId = (hitsV[0] >> 16) & 0x1F;
+    vector<unsigned long int> temp;
+
+    for(unsigned int cnt = 0; cnt<hitsV.size(); cnt++)
+      {
+	//std::cout << "LONG = " << std::bitset<32>(hitsV[cnt]) << std::endl;
+	//std::cout << "HITID = " << std::bitset<5>((hitsV[cnt] >> 16) & 0x1F) << std::endl;
+	if((hitsV[cnt] >> 16) & 0x1F == baseHitId)
+	  {
+	    temp.push_back(hitsV[cnt]);
+	  }
+	else
+	  {
+	    hitTimeVec.push_back(HitTime(temp));
+	    temp.clear();
+	    // Next hit
+	    baseHitId = (hitsV[cnt] >> 16) & 0x1F;
+	    temp.push_back(hitsV[cnt]);
+	  }
+
+      }
+
+    cout<<"Created HitTimeVec"<<endl;
+
+    vector<unsigned long int> eventV(longV.end()-5,longV.end()-3);
+
+    eventTrailer = EventTrailer(eventV);
+
+    cout<<"Created EventTrailer"<<endl;
+
+    vector<unsigned long int> spillV(longV.end()-3,longV.end());
+
+    spillTrailer = SpillTrailer(spillV);
+
+    cout<<"Created SpillTrailer"<<endl;
+    
+      }
+
+  };
+  // want to take in a vector to produce constructor?
+  SpillHeader header;
+  EventHeader eventHeader;
+  vector<HitTime> hitTimeVec;
+  EventTrailer eventTrailer;
+  SpillTrailer spillTrailer;
+
+  unsigned long int triggerTag;
+
+
+};
+
+
+
+
+
+
 
 // Union used to decode the FEB communication data fields
 typedef union
@@ -103,6 +343,130 @@ typedef union
     }EvD;
 } CommunicationField_t;       /* 4 bytes size */
 
+
+vector<bhep::hit*> ConvertToHit(HitStructure inStructure)
+{
+  vector<bhep::hit*> returnVector;
+
+
+// Read in 2 txt files that contains info needed to convert.
+// one for edep per channel per board
+// one for x/y z ybar, num 
+
+
+  for(unsigned int cnt=0;cnt<inStructure.hitTimeVec.size();cnt++)
+    {
+      for(unsigned int inCnt=0;inCnt<inStructure.hitTimeVec[cnt].hitAmplitudeVec.size();inCnt++)
+	{
+	  //ddata( "EnergyDep" )
+	  //inStructure.hitTimeVec[cnt].hitAmplitudeVec[inCnt].amplitudeMeasurement
+	  //inStructure.hitTimeVec[cnt].hitAmplitudeVec[inCnt].channelId
+	  //inStructure.spillHeader.boardId
+	  
+	  //ddata( "barPosZ" )
+	  //idata( "IsYBar" )
+	  //idata( "barNumber" )
+	  //ddata( "barPosT" )
+	  //inStructure.hitTimeVec[cnt].hitAmplitudeVec[inCnt].channelId
+	  //inStructure.spillHeader.boardId
+
+	  //ddata( "time" );
+	  //inStructure.hitTimeVec[cnt].hitTime
+	}
+
+    }
+
+
+  
+
+    //returnVector.back()->add_data("time",FieldTest.EvD.HitTime);
+    //returnVector.back()->add_property("time",(double)FieldTest.EvD.HitTime);
+
+    //cout<<"FieldTest.EvD.HitID: "<<FieldTest.EvD.HitID<<endl;
+
+    //returnVector.back().add_data(name,value);
+  
+
+  return returnVector;
+}
+
+static void print_element_names(xmlNode * a_node)
+{
+    xmlNode *cur_node = NULL;
+
+    for (cur_node = a_node; cur_node; cur_node = cur_node->next) {
+        if (cur_node->type == XML_ELEMENT_NODE) {
+            printf("node type: Element, name: %s\n", cur_node->name);
+
+	    xmlAttr* attribute = cur_node->properties;
+
+	    while(attribute)
+	      {
+		printf("node type: Element, properties: %s\n", attribute->name);
+		// if the attribute name ==...
+		// if that content == .. 
+		printf("node type: Element, content: %s\n", xmlNodeGetContent(attribute->children));
+		attribute = attribute->next;
+	      }
+
+
+        }
+
+        print_element_names(cur_node->children);
+    }
+}
+
+
+void parseXML(void)
+{
+    xmlDoc *doc = NULL;
+    xmlNode *root_element = NULL;
+
+
+    /*
+     * this initialize the library and check potential ABI mismatches
+     * between the version it was compiled for and the actual shared
+     * library used.
+     */
+    LIBXML_TEST_VERSION
+
+    /*parse the file and get the DOM */
+    doc = xmlReadFile("testXML.xml", NULL, 0);
+
+    if (doc == NULL) {
+        printf("error: could not parse file %s\n", "testXML.xml");
+    }
+
+    /*Get the root element node */
+    root_element = xmlDocGetRootElement(doc);
+    cout<<"Printing"<<endl;
+    print_element_names(root_element);
+    cout<<"End of print"<<endl;
+
+    //
+
+
+    /*free the document */
+    xmlFreeDoc(doc);
+
+    /*
+     *Free the global variables that may
+     *have been allocated by the parser.
+     */
+    xmlCleanupParser();
+
+
+
+
+
+
+
+}
+
+
+
+
+
 vector<bhep::hit*> HandleTestBeamData()//(char *sInputFileName)
 {
   vector<bhep::hit*> returnVector;
@@ -118,6 +482,8 @@ vector<bhep::hit*> HandleTestBeamData()//(char *sInputFileName)
   
   while(fread(&FieldTest.value, sizeof(unsigned int), 1, pDAQDataFile) == 1)
   {
+    cout<<"value"<<FieldTest.value<<endl;
+
     // Process
      /*
     fprintf(pTXTDataFile,
@@ -164,6 +530,161 @@ vector<bhep::hit*> HandleTestBeamData()//(char *sInputFileName)
 
   return returnVector;
 }
+
+vector<bhep::hit*> HandleTestBeamDataNew()//(char *sInputFileName)
+{
+  parseXML();
+
+  vector<bhep::hit*> returnVector;
+  
+  char buffer[20];
+
+  //unsigned char *data=new unsigned char [20];
+
+
+  //string inFile = "A2-1+2-HV135-HG55-THD280-TC1-HOLD6-1.daq";
+  
+  
+  //std::ifstream inFile("A2-1+2-HV135-HG55-THD280-TC1-HOLD6-1.daq",ios::binary | ios::in);
+  std::ifstream inFile("A2-1+2-HV135-HG55-THD280-TC1-HOLD6-1.daq",ios::binary |ios::in);
+
+  //std::locale loc("");
+  //cout<<std::locale("").name().c_str()<<endl;
+
+  //inFile.imbue(loc);
+
+  //inFile.read(buffer, sizeof(buffer));
+
+  //inFile.read(buffer, sizeof(buffer));
+
+  //inFile.read( (char*)data,20);
+  /*
+  while (!inFile.eof()) {
+    unsigned char a,b,c,d;
+    inFile >> a >> b >> c >> d;
+    std::cout <<std::hex
+	      <<static_cast<unsigned>(a)
+	      <<static_cast<unsigned>(b)
+	      <<static_cast<unsigned>(c)
+	      <<static_cast<unsigned>(d)
+	      <<endl;
+  }
+  */
+
+  
+  std::string line;
+  while (std::getline(inFile, line))
+    {
+    //cout<<line<<endl;
+      //cout<<line.length()<<endl;
+
+      cout<<std::hex<<line<<endl;
+
+      /*
+      for(int i = 0; i<line.size();i+=4)
+	{
+	  cout<<std::bitset<4>(atoi(line.substr(i,1).c_str()))
+	      <<std::bitset<4>(atoi(line.substr(i+1,1).c_str()))
+	      <<std::bitset<4>(atoi(line.substr(i+2,1).c_str()))
+	      <<std::bitset<4>(atoi(line.substr(i+3,1).c_str()))<<endl;
+	}
+      */
+
+
+      //cout<<line.length()<<endl;
+      
+      std::vector<char> bytes(line.begin(), line.end());
+
+      //std::vector<unsigned short> bytesInt(line.begin(), line.end());
+      /*
+      cout<<"Begin"<<endl;
+      for(int i = 0; i<bytesInt.size();i+=4)
+	{
+	  cout<<std::hex<<bytes[i]<<endl;
+	  cout<<std::hex<<bytes[i+1]<<endl;
+	  cout<<std::hex<<bytes[i+2]<<endl;
+	  cout<<std::hex<<bytes[i+3]<<endl;
+	  cout<<"done"<<endl;
+	  
+	  cout<<std::bitset<8>(bytes[i])
+	      <<std::bitset<8>(bytes[i+1])
+	      <<std::bitset<8>(bytes[i+2])
+	      <<std::bitset<8>(bytes[i+3])
+	      <<endl;
+	  
+	}
+      cout<<"END"<<endl;
+      */
+
+
+      /*
+      cout<<"Begin"<<endl;
+      for(int i = 0; i<bytesInt.size();i+=4)
+	{
+	  cout<<std::hex<<bytesInt[i]<<endl;
+	  cout<<std::hex<<bytesInt[i+1]<<endl;
+	  cout<<std::hex<<bytesInt[i+2]<<endl;
+	  cout<<std::hex<<bytesInt[i+3]<<endl;
+	  cout<<"done"<<endl;
+	  
+	  cout<<std::bitset<8>(bytesInt[i])
+	      <<std::bitset<8>(bytesInt[i+1])
+	      <<std::bitset<8>(bytesInt[i+2])
+	      <<std::bitset<8>(bytesInt[i+3])
+	      <<endl;
+	  
+	}
+      cout<<"END"<<endl;
+      */
+
+
+      //cout<<"TESTING"<<endl;
+      //cout<<bytes[0]<<bytes[1]<<bytes[2]<<bytes[3]<<endl;
+      //cout<<"END TESTING"<<endl;
+
+      //cout<<std::bitset<8>(bytes[0])<<std::bitset<8>(bytes[1])<<std::bitset<8>(bytes[2])<<std::bitset<8>(bytes[3])<<endl;
+      //cout<<bytes.size()<<endl;
+      //cout<<bytes[0]<<endl;
+      //std::cout << "LONG = " << std::bitset<8>(bytes[0]) << std::endl;
+
+  
+      std::vector<unsigned long int> longV;
+
+      for(int i = 0; i<bytes.size();i+=4)
+	{
+	  /*
+	  cout<<bytes[i]<<bytes[i+1]<<bytes[i+2]<<bytes[i+3]<<endl;
+	  cout<<std::bitset<8>(bytes[i])
+	      <<std::bitset<8>(bytes[i+1])
+	      <<std::bitset<8>(bytes[i+2])
+	      <<std::bitset<8>(bytes[i+3])
+	      <<endl;
+	  */
+	  longV.push_back(bytes[i] << 24 | bytes[i+1] << 16 |  bytes[i+2] << 8 | bytes[i+3] << 0);
+
+	  //cout<<longV.back()<<endl;
+	  //cout<<std::bitset<32>(longV.back())<<endl;
+	}
+  
+      HitStructure test(longV);
+
+      //cout<<longV.size()<<endl;
+      //cout<<longV[0]<<endl;
+
+      //longV.clear();
+	
+      //cout<<0xFF<<endl;
+      //cout<<(0xFF >> 2)<<endl;
+      //cout<<((0xFF >> 4) & 0x7) <<endl;
+
+      //std::cout << "LONG = " << std::bitset<32>(longV[0]) << std::endl;
+      //std::cout << "LONG = " << std::bitset<4>(longV[0]) << std::endl;
+      
+  }
+  
+  return returnVector;
+}
+
 
 int main(int argc, char* argv[]) {
   
@@ -257,6 +778,8 @@ int main(int argc, char* argv[]) {
       HandleTestBeamData();
     }
   */
+  //(void) HandleTestBeamData();
+  (void) HandleTestBeamDataNew();
 
   //vector<bhep::hit*> testVec = HandleTestBeamData();
   /*
--- pythonlib/xml_AiDA_parser.py
+++ pythonlib/xml_AiDA_parser.py
@@ -23,12 +23,72 @@ class xml_AiDA_parser:
         self.xmldoc = minidom.parse(self.parsingFile)
 
     def Parse_file(self):
+
+        physNodeVolRefList = []
+        initZPosList = []
+        replicaList = []
+        widthList = []
+        barLengthList = []
+        centerZPosList = []
+
+        
+        worldRef = self.xmldoc.getElementsByTagName('setup')[0].getElementsByTagName('world')[0].attributes['ref'].value
+
+        #print worldRef
+
+        volumeNodeList = self.xmldoc.getElementsByTagName('volume')
+
+        for volumeNode in volumeNodeList:
+            if(volumeNode.attributes['name'].value == worldRef):
+                physNodeList=volumeNode.getElementsByTagName('physvol')
+                if(len(physNodeList) ==1): #If 1, old gdml format, 2 repeating structure.
+                    old = 1
+                else :
+                    old = 0
+
+        if(old == 0):
+            #Find the initial and center z positions from the physical volumes.
+            for physNode in physNodeList:
+                #print physNode.getElementsByTagName('volumeref')[0].attributes['ref'].value
+                physNodeVolRefList.append(physNode.getElementsByTagName('volumeref')[0].attributes['ref'].value)
+                #print physNode.getElementsByTagName('volumeref')[0].attributes['ref'].value[-8:]
+                if(physNode.getElementsByTagName('volumeref')[0].attributes['ref'].value[-8:] != 'Detector'):
+                    initZPosList.append(physNode.getElementsByTagName('position')[0].attributes['z'].value)
+                else:
+                    centerZPosList.append(physNode.getElementsByTagName('position')[0].attributes['z'].value)
+
+
+            #Find the number of replica volumes, z width of modules and bar xy lengths from the physical volume names.
+            for physNodeVolRef in physNodeVolRefList:
+                for volumeNode in volumeNodeList:
+                    if(volumeNode.attributes['name'].value == physNodeVolRef):
+                        if volumeNode.getElementsByTagName("replicavol"):
+                            replicaList.append(volumeNode.getElementsByTagName("replicavol")[0].attributes['number'].value)
+                        if volumeNode.getElementsByTagName("width"):
+                            widthList.append(volumeNode.getElementsByTagName("width")[0].attributes['value'].value)
+                        auxiliaryList= volumeNode.getElementsByTagName("auxiliary")
+                        for auxiliary in auxiliaryList:
+                            if(auxiliary.attributes['auxtype'].value == 'BarLength'):
+                                barLengthList.append(auxiliary.attributes['auxvalue'].value)
+
+
+            if(len(initZPosList)!=len(replicaList)):
+                print 'Can not parse this gdml file'
+            else:
+                zlength = abs(eval(initZPosList[0]))*2*10
+                xlength = eval(barLengthList[0])*10
+                ylength = eval(barLengthList[0])*10
+                outfile = open(self.outFile,'w+')
+                outfile.write("SOLID\t%s\t%s\t%s\t%s\n"%('S',xlength,ylength,zlength))
+                outfile.write("POS\t%s\t%s\t%s\t%s\n"%('S',0,0,centerZPosList[0]))
+                outfile.close()
+
+        if(old == 1):
+            self.Old_parse_file()
+                        
+    def Old_parse_file(self):
         #Variables used
         volumeNodeList = self.xmldoc.getElementsByTagName('volume')
-        #print 'volumeNodeList'
-        #print volumeNodeList
-        #print 'done'
-        
         solidRefRefList = []
         volumeRefRefList = []
 
@@ -55,28 +115,50 @@ class xml_AiDA_parser:
 
                 continue
 
+        
+        #Get volume ref & position name
         for volumeNode in volumeNodeList:
-            if(volumeNode.attributes['name'].value == 'EMRDetector'):
-                replicaVolList= volumeNode.getElementsByTagName("replicavol")
-                auxiliaryList= volumeNode.getElementsByTagName("auxiliary")
-                 
-            if(volumeNode.attributes['name'].value == 'EMR'):
-                posRefList= volumeNode.getElementsByTagName("position")
-                 
-            
+            if(volumeNode.attributes['name'].value == 'Detector'):
+                volumeRefList= volumeNode.getElementsByTagName("volumeref")
+                for volumeRef in volumeRefList:
+                    volumeRefRefList.append(volumeRef.attributes['ref'].value)
+                posNameRefList= volumeNode.getElementsByTagName("position")
+                for posNameRef in posNameRefList:
+                    name_val = str(posNameRef.attributes['name'].value)
+                    x_val = eval(str(posNameRef.attributes['x'].value),constantDict)
+                    y_val = eval(str(posNameRef.attributes['y'].value),constantDict)
+                    z_val = eval(str(posNameRef.attributes['z'].value),constantDict)
+                    posDict[name_val] = ((x_val,y_val,z_val))
+                    
+        #Get solid ref
+        for volumeRefRef in volumeRefRefList: 
+            for volumeNode in volumeNodeList:
+                if(volumeNode.attributes['name'].value == volumeRefRef):
+                    solidRefList= volumeNode.getElementsByTagName("solidref")
+                    for solidRef in solidRefList:
+                        solidRefRefList.append(solidRef.attributes['ref'].value)
 
-        print replicaVolList[0].attributes['number'].value
-        for auxiliary in auxiliaryList:
-            if(auxiliary.attributes['auxtype'].value == 'BarLength'):
-                print auxiliary.attributes['auxvalue'].value
-        print posRefList[0].attributes['z'].value
-                      
-
-
+        #Get solid ref xyz
+        for solidRefRef in solidRefRefList: 
+            for boxNode in boxNodeList:
+                if(boxNode.attributes['name'].value == solidRefRef):
+                    x_val = eval(str(boxNode.attributes['x'].value),constantDict)
+                    y_val = eval(str(boxNode.attributes['y'].value),constantDict)
+                    z_val = eval(str(boxNode.attributes['z'].value),constantDict)
+                    boxDict[str(solidRefRef)] = ((x_val,y_val,z_val))
                 
         outfile = open(self.outFile,'w+')
-        outfile.write("SOLID\t%s\t%s\t%s\t%s\n"%('S',1000,1000,195*2))
-        outfile.write("POS\t%s\t%s\t%s\t%s\n"%('S',0,0,0))
-        outfile.close()
+        for key,value in boxDict.iteritems():
+            outfile.write("SOLID\t%s\t%s\t%s\t%s\n"%(key[:-6],value[0],value[1],value[2]))
+        for key,value in posDict.iteritems():
+            outfile.write("POS\t%s\t%s\t%s\t%s\n"%(key[:-4],value[0],value[1],value[2]))
+        outfile.close()                   
+
+
+
+
+    
+
+
 
 #######################################################################################################################
